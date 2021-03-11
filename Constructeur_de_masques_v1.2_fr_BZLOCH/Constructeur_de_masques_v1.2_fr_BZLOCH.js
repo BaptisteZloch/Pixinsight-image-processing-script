105cc9026b03703a512869ffbf74db6d215a4d7c
Baptiste ZLOCH <72359933+BaptisteZloch@users.noreply.github.com>
2021-03-11 11:03:05 +0100

    Update Constructeur_de_masques_v1.2_fr_BZLOCH.js

--- Constructeur_de_masques_v1.2_fr_BZLOCH/Constructeur_de_masques_v1.2_fr_BZLOCH.js
+++ Constructeur_de_masques_v1.2_fr_BZLOCH/Constructeur_de_masques_v1.2_fr_BZLOCH.js
@@ -16,7 +16,7 @@
 Un script qui permet de faire tous les types de masques en automatique.< br />\
 <br />\
 Copyright & copy; 2020 Baptiste ZLOCH
-#feature - icon  ZTA.xpm
+#feature - icon  ZTA.svg
 //define des textes
 #define VERSION "1.2"
 #define TITLE "Constructeur de masques v" + VERSION
